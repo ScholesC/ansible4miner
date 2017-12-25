94a5e2a977b34fc256adaec316e6acda501fb9de
dchen <skles.chh@gmail.com>
2017-12-26 07:45:44 +0800

    add while loop to start miner

--- roles/os_init/templates/miner/startup.sh
+++ roles/os_init/templates/miner/startup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-sleep 1m
-
-
-
-
-#cd /home/eth/ClaymoreMiner && sh run.sh
-#cd /home/eth/eth && sh /home/eth/eth/start.bash
-
-cd  /home/eth/zec/ && ./run.sh
+#
+# configure GPU
+#
 gpu_type=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | grep -oP "(?<=\s)\d{2,}" | sort -u)
 powerlimit=120
 
@@ -21,3 +15,16 @@ case $gpu_type in
 esac
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl $powerlimit
+
+# start miner
+while true
+do
+    if [[ $(nvidia-smi --query-compute-apps=process_name --format=csv,noheader | wc -l) -lt 1 ]]
+    then
+        #cd /home/eth/ClaymoreMiner && sh run.sh
+        #cd /home/eth/eth && sh /home/eth/eth/start.bash
+        cd  /home/eth/zec/ && ./run.sh
+        #cd /home/eth/ccminer && ./run.sh
+    fi
+    sleep 30
+done
