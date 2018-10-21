7b43d64ca0f45da1e7a89a3411a29e964bece062
dchen <skles.chh@gmail.com>
2018-10-21 23:28:08 +0800

    new renew_hosts_file.sh

--- renew_hosts_file.sh
+++ renew_hosts_file.sh
@@ -4,40 +4,74 @@
 # Create Date: 2017-10-15 19:50
 # Modify Date: 2017-10-15 19:50
 #***************************************************************#
-cat /dev/null > /home/eth/.ssh/known_hosts
+export PATH=/usr/bin:/usr/sbin:/bin:/sbin
+hostsfile=/home/eth/hosts
+if ! grep -P "127.0.0.1\s+$HOSTNAME\s+localhost" $hostsfile
+then
+  echo "127.0.0.1  $HOSTNAME localhost" >> $hostsfile
+fi
 int_name=$(ip -o addr | grep -oP "eth\d+" | sort -u)
 IP_LIST=$(sudo arp-scan --interface $int_name --localnet | egrep ^192.168 | awk '{print $1}')
-#IP_LIST=$(sudo arp-scan --interface eth1 --localnet | egrep ^192.168 | awk '{print $1}')
 PSSH_DIR=/tmp/PSSHDIR_$$
 mkdir $PSSH_DIR
-#pssh -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
 #echo 23835261 | sshpass -ppassword pssh -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
-sshpass -f /home/eth/remotePasswordFile pssh -O " PreferredAuthentications=password" -O " PubkeyAuthentication=no" -A -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
+pssh -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
+empty_file_list=$(find $PSSH_DIR -empty -printf "%f\n")
+sshpass -f /home/eth/remotePasswordFile pssh -O " PreferredAuthentications=password" -O " PubkeyAuthentication=no" -A -o $PSSH_DIR -t 5 -p 100 -H "$empty_file_list" 'uname -n' &>/dev/null
 num_of_hosts=$(find $PSSH_DIR ! -empty | wc -l)
 #if [[ $num_of_hosts -gt 10 ]]
 #then
-cat /dev/null > /home/eth/hosts 
-echo "127.0.0.1  $HOSTNAME localhost" >> /home/eth/hosts
+#cat /dev/null > /home/eth/hosts 
+cat /dev/null > /home/eth/.ssh/known_hosts
+
 for IP in $(ls -1 $PSSH_DIR)
 do
   HOST_NAME=$(cat $PSSH_DIR/$IP)
-  if [[ -n $HOST_NAME ]]
+#  if [[ -n $HOST_NAME ]]
+#  then
+#    OLD_IP=$(egrep $HOST_NAME /etc/hosts.base | awk '{print $1}' | sort -u) 
+#  else
+#    OLD_IP='NONE'
+#  fi
+  #echo  "$IP    $HOST_NAME    # $OLD_IP " >> /home/eth/hosts
+  
+  host_entry_name=$(grep -P "$IP " $hostsfile | awk '{print $2}')
+  if ! grep -P "$IP\s+$HOST_NAME " $hostsfile
   then
-    OLD_IP=$(egrep $HOST_NAME /etc/hosts.base | awk '{print $1}' | sort -u) 
+    if [[ -n $HOST_NAME ]]
+    then
+#      echo "$IP - $HOST_NAME - $host_entry_name"
+      if [[ $HOST_NAME != $host_entry_name ]]
+      then
+        #echo "doesn't match"
+        sed -i "/ $HOST_NAME /d" $hostsfile
+        sed -i "/$IP /d" $hostsfile
+        echo  "$IP    $HOST_NAME " >> $hostsfile
+        #sed -i "/$IP / s|$host_entry_name|$HOST_NAME|g" $hostsfile
+      fi
+    #else
+    #  echo  "$IP    $HOST_NAME " >> $hostsfile
+    fi
   else
-    OLD_IP='NONE'
+    if [[ -n $HOST_NAME ]]
+    then
+      if [[ $(grep -P " $HOST_NAME " $hostsfile | wc -l) -gt 1 ]]
+      then
+        sed -i "/ $HOST_NAME /d" $hostsfile
+        echo  "$IP    $HOST_NAME " >> $hostsfile 
+      fi
+    fi
   fi
-  echo  "$IP    $HOST_NAME    # $OLD_IP " >> /home/eth/hosts
 done
-while read IP HOST_NAME
-do
-  if [[ -n $HOST_NAME ]]
-  then
-    [[ $(egrep -w $HOST_NAME /home/eth/hosts) ]] || echo "#$IP    $HOST_NAME     # OLD hosts " >> /home/eth/hosts
-  fi
-done</etc/hosts.base
+#while read IP HOST_NAME
+#do
+#  if [[ -n $HOST_NAME ]]
+#  then
+#    [[ $(egrep -w $HOST_NAME /home/eth/hosts) ]] || echo "#$IP    $HOST_NAME     # OLD hosts " >> /home/eth/hosts
+#  fi
+#done</etc/hosts.base
 #fi
 
 rm -rf $PSSH_DIR
 
-grep -P "^\d+" /home/eth/hosts |  awk '/^[^#]/{print $1}' > /home/eth/hosts.ip
+grep -P "^\d+" /home/eth/hosts | grep -v " $HOSTNAME " | awk '/^[^#]/{print $1}' > /home/eth/hosts.ip
