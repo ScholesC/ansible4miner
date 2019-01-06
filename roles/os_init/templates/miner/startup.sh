d853b0e25d01675258d71f2299668a7e90e84869
陈昊 <chenhao@s-MacBook-Pro.local>
2019-01-06 08:52:34 +0800

    change default power limit

--- roles/os_init/templates/miner/startup.sh
+++ roles/os_init/templates/miner/startup.sh
@@ -11,10 +11,10 @@ sudo nvidia-smi -pm 1
 while read index gpu_type
 do
     echo $index $gpu_type
-    egrep 1080 <<<$gpu_type && powerlimit=230
-    egrep 1070 <<<$gpu_type && powerlimit=160
-    egrep 1060 <<<$gpu_type && powerlimit=120
-    egrep P106 <<<$gpu_type && powerlimit=120
+    egrep 1080 <<<$gpu_type && powerlimit=210
+    egrep 1070 <<<$gpu_type && powerlimit=140
+    egrep 1060 <<<$gpu_type && powerlimit=100
+    egrep P106 <<<$gpu_type && powerlimit=100
     sudo nvidia-smi -i $index -pl $powerlimit
 done< <(nvidia-smi --query-gpu=index,gpu_name --format=csv,noheader | sed 's|,| |g')
 
@@ -23,7 +23,8 @@ sleep 30
 # start miner
 #cd /home/eth/ClaymoreMiner && sh run.sh
 #cd /home/eth/eth && sh /home/eth/eth/start.bash
-cd  /home/eth/zec/ && ./run.sh
+#cd  /home/eth/zec/ && ./run.sh
 #cd /home/eth/ccminer && ./run.sh
 #cd /home/eth/eth && ./run.sh
 #cd /home/eth/btm && ./run.sh
+cd /home/eth/hspminer && ./run.sh
