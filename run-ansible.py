162da8565cebab4d1faa344aaaa92438dbc01aed
David Chen <skles.chh@gmail.com>
2019-03-09 14:27:44 +0800

    bugfix

--- run-ansible.py
+++ run-ansible.py
@@ -186,7 +186,7 @@ def main():
         os.system('pssh -i -H "{0}" "rm -rvf /home/eth/{1}"'.format(server_list, m_type))
         os.system('echo {0} > miners/{1}/{1}/address.txt'.format(m_addr, m_type))
         os.system('prsync -av -H "{0}" miners/{1}/ /home/eth/'.format(server_list, m_type))
-    os.system('pssh -i -H "{0}" "sudo reboot"'.format(server_list, m_type))
+    os.system('pssh -i -H "{0}" "sudo reboot"'.format(server_list))
 
 
 if __name__ == "__main__":
