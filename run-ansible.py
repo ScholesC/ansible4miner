fd3df35ed9ed53544cdd362d25b0d17f62d1612c
David Chen <skles.chh@gmail.com>
2019-03-24 13:11:10 +0800

    new function: deploy miner app thru ansible instead of pssh

--- run-ansible.py
+++ run-ansible.py
@@ -176,20 +176,19 @@ def main():
     m_type = args.m_type
     m_addr = args.m_addr
     m_dtype = args.m_dtype
-    print(servers)
-    playbook_path='/home/eth/ansible/os_init.yml'
     extra_data={}
-    extra_data['ansible_sudo_pass']=password
-    extra_data['ansible_ssh_pass']=password
+    extra_data['ansible_sudo_pass'] = password
+    extra_data['ansible_ssh_pass'] = password
     server_list = " ".join(servers)
+    print(servers)
     if m_dtype == "os_init" or m_dtype == "all":
+        playbook_path='/home/eth/ansible/os_init.yml'
         run_playbook(servers,playbook_path,extra_data)
     if m_addr and m_type:
-        os.system('pssh -i -H "{0}" "sudo pkill -f pool"'.format(server_list))
-        os.system('pssh -i -H "{0}" "rm -rvf /home/eth/{1}"'.format(server_list, m_type))
-        os.system('echo {0} > miners/{1}/{1}/address.txt'.format(m_addr, m_type))
-        os.system('prsync -av -H "{0}" miners/{1}/ /home/eth/'.format(server_list, m_type))
-        os.system('pssh -i -H "{0}" "cd /home/eth/{1} && ./run.sh"'.format(server_list, m_type))
+        extra_data['miner_name'] = m_type
+        extra_data['miner_addr'] = m_addr
+        playbook_path='/home/eth/ansible/miners.yml'
+        run_playbook(servers,playbook_path,extra_data)
 
 if __name__ == "__main__":
     try:
---
+++ scripts/renew_hosts_file.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#****************************************************************#
+# ScriptName: renew_hosts_file.sh
+# Create Date: 2017-10-15 19:50
+# Modify Date: 2017-10-15 19:50
+#***************************************************************#
+export PATH=/usr/bin:/usr/sbin:/bin:/sbin
+hostsfile=/home/eth/hosts
+if ! grep -P "127.0.0.1\s+$HOSTNAME\s+localhost" $hostsfile
+then
+  echo "127.0.0.1  $HOSTNAME localhost" >> $hostsfile
+fi
+int_name=$(ip -o addr | grep -oP "eth\d+" | sort -u)
+IP_LIST=$(sudo arp-scan --interface $int_name --localnet | egrep ^192.168 | awk '{print $1}')
+PSSH_DIR=/tmp/PSSHDIR_$$
+mkdir $PSSH_DIR
+#echo 23835261 | sshpass -ppassword pssh -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
+pssh -o $PSSH_DIR -t 5 -p 100 -H "$IP_LIST" 'uname -n' &>/dev/null
+empty_file_list=$(find $PSSH_DIR -empty -printf "%f\n")
+sshpass -f /home/eth/remotePasswordFile pssh -O " PreferredAuthentications=password" -O " PubkeyAuthentication=no" -A -o $PSSH_DIR -t 5 -p 100 -H "$empty_file_list" 'uname -n' &>/dev/null
+num_of_hosts=$(find $PSSH_DIR ! -empty | wc -l)
+#if [[ $num_of_hosts -gt 10 ]]
+#then
+#cat /dev/null > /home/eth/hosts 
+cat /dev/null > /home/eth/.ssh/known_hosts
+
+for IP in $(ls -1 $PSSH_DIR)
+do
+  HOST_NAME=$(cat $PSSH_DIR/$IP)
+#  if [[ -n $HOST_NAME ]]
+#  then
+#    OLD_IP=$(egrep $HOST_NAME /etc/hosts.base | awk '{print $1}' | sort -u) 
+#  else
+#    OLD_IP='NONE'
+#  fi
+  #echo  "$IP    $HOST_NAME    # $OLD_IP " >> /home/eth/hosts
+  
+  host_entry_name=$(grep -P "$IP " $hostsfile | awk '{print $2}')
+  if ! grep -P "$IP\s+$HOST_NAME " $hostsfile
+  then
+    if [[ -n $HOST_NAME ]]
+    then
+#      echo "$IP - $HOST_NAME - $host_entry_name"
+      if [[ $HOST_NAME != $host_entry_name ]]
+      then
+        #echo "doesn't match"
+        sed -i "/ $HOST_NAME /d" $hostsfile
+        sed -i "/$IP /d" $hostsfile
+        echo  "$IP    $HOST_NAME " >> $hostsfile
+        #sed -i "/$IP / s|$host_entry_name|$HOST_NAME|g" $hostsfile
+      fi
+    #else
+    #  echo  "$IP    $HOST_NAME " >> $hostsfile
+    fi
+  else
+    if [[ -n $HOST_NAME ]]
+    then
+      #echo "$HOST_NAME ck"
+      if [[ $(grep -w "$HOST_NAME" $hostsfile | wc -l) -gt 1 ]]
+      then
+        for IPX in $(grep -w "$HOST_NAME" $hostsfile | awk '{print $1}')
+        do
+	  #echo "$HOST_NAME $IPX ck1"
+          IPX_HOSTNAME=$(cat $PSSH_DIR/$IPX)
+          if  [[ $HOST_NAME != $IPX_HOSTNAME ]]
+          then
+            echo "$IPX $HOST_NAME $IPX_HOSTNAME ck2"
+            perl -n -i -e "print unless m/$IPX\s+$HOST_NAME\s+/" $hostsfile
+          fi
+        done
+      fi
+    fi
+  fi
+done
+#while read IP HOST_NAME
+#do
+#  if [[ -n $HOST_NAME ]]
+#  then
+#    [[ $(egrep -w $HOST_NAME /home/eth/hosts) ]] || echo "#$IP    $HOST_NAME     # OLD hosts " >> /home/eth/hosts
+#  fi
+#done</etc/hosts.base
+#fi
+
+rm -rf $PSSH_DIR
+
+grep -P "^\d+" /home/eth/hosts | grep -v " $HOSTNAME " | awk '/^[^#]/{print $1}' > /home/eth/hosts.ip
