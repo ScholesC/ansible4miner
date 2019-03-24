931f6708fd5208548d9c5421c77d84b15c1649b9
David Chen <skles.chh@gmail.com>
2019-03-24 18:07:44 +0800

    delete some files

--- git-pull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-#
-cd /home/eth/ansible && git pull
--- mirror-miners.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-#
-rsync -av --include="cuda/*" --include="package/*" --include="minersapp/*" --include="miners/*" root@download.skles.com:/var/www/html/download/ansible/ /home/eth/ansible/
