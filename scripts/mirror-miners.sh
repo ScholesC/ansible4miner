7f8e680e2a66b08bddadcce112770cf3bd6246a1
David Chen <skles.chh@gmail.com>
2019-03-25 22:14:40 +0800

    rsync compress with -z

--- scripts/mirror-miners.sh
+++ scripts/mirror-miners.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 #
-rsync -av --include="cuda/*" --include="package/*" --include="minersapp/*" --include="miners/*" root@download.skles.com:/var/www/html/download/ansible/ /home/eth/ansible/
+rsync -avz --include="cuda/*" --include="package/*" --include="minersapp/*" --include="miners/*" root@download.skles.com:/var/www/html/download/ansible/ /home/eth/ansible/
