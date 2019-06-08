185060eab0ed1f94bd6a7928c6f8a770492e7ccc
David Chen <skles.chh@gmail.com>
2019-06-08 17:25:50 +0800

    adding p102 and p104 in startup.sh

--- roles/os_init/templates/miner/startup.sh
+++ roles/os_init/templates/miner/startup.sh
@@ -12,7 +12,9 @@ while read index gpu_type
 do
     echo $index $gpu_type
     egrep 1080 <<<$gpu_type && powerlimit=200
+    egrep p102 <<<$gpu_type && powerlimit=200
     egrep 1070 <<<$gpu_type && powerlimit=140
+    egrep p104 <<<$gpu_type && powerlimit=140
     egrep 1060 <<<$gpu_type && powerlimit=100
     egrep P106 <<<$gpu_type && powerlimit=100
     sudo nvidia-smi -i $index -pl $powerlimit
