9b74b631e6151b28d68dddac1707f58f70afc595
David Chen <skles.chh@gmail.com>
2019-06-16 22:50:35 +0800

    bugfix in run.sh

--- roles/os_init/templates/miner/startup.sh
+++ roles/os_init/templates/miner/startup.sh
@@ -12,11 +12,11 @@ while read index gpu_type
 do
     echo $index $gpu_type
     egrep 1080 <<<$gpu_type && powerlimit=200
-    egrep p102 <<<$gpu_type && powerlimit=210
+    egrep -i p102 <<<$gpu_type && powerlimit=210
     egrep 1070 <<<$gpu_type && powerlimit=140
-    egrep p104 <<<$gpu_type && powerlimit=150
+    egrep -i p104 <<<$gpu_type && powerlimit=150
     egrep 1060 <<<$gpu_type && powerlimit=100
-    egrep P106 <<<$gpu_type && powerlimit=100
+    egrep -i P106 <<<$gpu_type && powerlimit=100
     sudo nvidia-smi -i $index -pl $powerlimit
 done< <(nvidia-smi --query-gpu=index,gpu_name --format=csv,noheader | sed 's|,| |g')
 
