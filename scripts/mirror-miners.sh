69529c19a77baf8d60ff8da4a44d0bce90661a6b
ScholesC <ScholesC@users.noreply.github.com>
2020-03-14 11:39:48 +0800

    Update mirror-miners.sh

--- scripts/mirror-miners.sh
+++ scripts/mirror-miners.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #
 #
-rsync -avz --include="cuda/*" --include="package/*" --include="minersapp/*" --include="miners/*" root@download.skles.com:/var/www/html/download/ansible/ /home/eth/ansible/
+timenow=$(date +%H%m)
+if [[ $timenow > 0000 ]] && [[ $timenow << 0200 ]]
+then
+  rsync -avz --include="cuda/*" --include="package/*" --include="minersapp/*" --include="miners/*" root@download.skles.com:/var/www/html/download/ansible/ /home/eth/ansible/
+else
+  rsync -avz root@download.skles.com:/var/www/html/download/ansible/miners/ /home/eth/ansible/miners/
+fi
